eefc8425d4971a20fe9f0f5dd6f0d34df1772db7
Jaylan <1419349524@qq.com>
2025-01-04 17:20:14 +0800

    Init

---
+++ CRMP/src/main/java/com/jaylan/crmp/CrmpApplication.java
@@ -0,0 +1,13 @@
+package com.jaylan.crmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmpApplication.class, args);
+    }
+
+}
---
+++ CRMP/src/test/java/com/jaylan/crmp/CrmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaylan.crmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
